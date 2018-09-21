d39fd1de5e7c4379162de0df7de6847accf8f123
zafir <ahmadzafir01@gmail.com>
2018-09-21 18:45:50 +0530

    Application first Commit

---
+++ app/core/App.php
@@ -0,0 +1,14 @@
+<?php 
+	class App{
+		protected $controller = 'home';
+		protected $method = 'index';
+		protected $params = [];
+
+		public function __construct(){
+			
+		}
+	}
+
+
+
+ ?>
---
+++ app/core/Controller.php
@@ -0,0 +1,8 @@
+<?php 
+	class Controller{
+		public function __construct(){
+			echo "Hey I am inside Controller";
+		}
+	}
+
+ ?>
